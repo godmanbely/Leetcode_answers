a27a0ae5b57c4bdd1e9b4ca2f8d2828b1870ff74
godmanbely <wangxiaochang1996@126.com>
2019-11-04 19:22:57 +0800

    提交中序遍历代码

--- src/com/leetcode/day3/ClimbStairs.java
+++ src/com/leetcode/day3/ClimbStairs.java
@@ -11,7 +11,7 @@ public class ClimbStairs {
 		for (int i = 2; i < n; i++) {
 			dp[i] = dp[i-1] + dp[i-2];
 		}
-		return dp[n-2];
+		return dp[n - 1];
 	}
 
 }
---
+++ src/com/leetcode/day3/InOrderTravesal.java
@@ -0,0 +1,49 @@
+package com.leetcode.day3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class InOrderTravesal {
+	public List<Integer> inOrderTravesal(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		helper(root, res);
+		return res;
+	}
+	private void helper(TreeNode root, List<Integer> res) {
+		if (root != null ) {
+			if (root.left != null) {
+				helper(root.left, res);
+			}
+			res.add(root.val);
+			if (root.right != null) {
+				helper(root.right, res);
+			}
+		}
+	}
+	
+	
+	public List<Integer> inOrderTravesal2(TreeNode root) {
+		List<Integer> res = new ArrayList<>();
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode curr = root;
+		while (curr != null || !stack.isEmpty()) {
+			while (curr != null) {
+				stack.push(curr);
+				curr = curr.left;
+			}
+			curr = stack.pop();
+			res.add(curr.val);
+			curr = curr.right;
+		}
+		return res;
+	}
+}
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){
+		this.val = x;
+	}
+}
